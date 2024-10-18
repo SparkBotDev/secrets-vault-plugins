0b69f73d3728970350c279e05fb6d6fc29a34a9b
Brian Lister <brian@sparkforge.dev>
2024-10-18 15:04:00 -0500

    feat: refactor to allow multiple secrets
    This is to leverage AWS rotating secrets for RDS

--- packages/aws-secrets-manager/src/plugin.ts
+++ packages/aws-secrets-manager/src/plugin.ts
@@ -1,50 +1,83 @@
 import { SecretsVault } from '@sparkbot/plugin-secrets';
 import {
 	SecretsManagerClient,
-	GetSecretValueCommand,
-	type GetSecretValueCommandOutput,
+	type BatchGetSecretValueCommandOutput,
+	BatchGetSecretValueCommand,
 } from '@aws-sdk/client-secrets-manager';
 import * as v from 'valibot';
+import type { AWSRegions } from './regions';
 
-const SecretsSchema = v.record(v.string(), v.string());
+const SecretsSchema = v.record(
+	v.string(),
+	v.pipe(v.union([v.string(), v.number()]), v.transform(String)),
+);
 
 export class SecretsVaultPlugin extends SecretsVault {
-	secrets: Promise<GetSecretValueCommandOutput>;
+	awsSecretsBatch: Promise<BatchGetSecretValueCommandOutput>;
+	cachedSecrets = new Map<string, Map<string, string>>();
+	isCached = false;
 
-	constructor(override readonly options: { secretName: string }) {
+	constructor(
+		override readonly options: { region: AWSRegions; secretIDs: string[] },
+	) {
 		super();
 		const client = new SecretsManagerClient({
-			region: 'us-east-1',
+			region: options.region,
 		});
-		this.secrets = client.send(
-			new GetSecretValueCommand({
-				SecretId: options.secretName, // eslint-disable-line @typescript-eslint/naming-convention
-				VersionStage: 'AWSCURRENT', // eslint-disable-line @typescript-eslint/naming-convention
+		this.awsSecretsBatch = client.send(
+			new BatchGetSecretValueCommand({
+				SecretIdList: options.secretIDs, // eslint-disable-line @typescript-eslint/naming-convention
 			}),
 		);
 	}
 
-	async get(key: string) {
-		let secrets: GetSecretValueCommandOutput;
-		try {
-			secrets = await this.secrets;
-		} catch (exception) {
-			throw new Error('AWS Secrets Manager Error', { cause: exception });
-		}
+	async get(key: string): Promise<string> {
+		const requestedKey = key.split('/').at(-1);
+		const requestedSecret = key.split('/').slice(0, -1).join('/');
+		if (!requestedKey || !requestedSecret)
+			throw new Error('Invalid key format');
 
-		if (secrets.SecretString) {
-			const parsedSecrets = v.safeParse(
-				SecretsSchema,
-				JSON.parse(secrets.SecretString),
-			);
-			if (parsedSecrets.success && parsedSecrets.output[key]) {
-				return parsedSecrets.output[key];
+		// If secrets from AWS have already been cached use them.
+		if (this.isCached) {
+			if (this.cachedSecrets.has(requestedSecret)) {
+				const value = this.cachedSecrets
+					.get(requestedSecret)
+					?.get(requestedKey);
+				if (value) {
+					return value;
+				}
 			}
 
-			throw new Error('Invalid Secret Format');
+			throw new Error(`Secret ${key} not found in secret cache`);
 		}
 
-		throw new Error('Invalid Secret Format');
+		// Wait for the secrets to be fetched from AWS and then cache them.
+		const awsSecrets = await this.awsSecretsBatch;
+		if (awsSecrets.Errors && awsSecrets.Errors?.length > 0)
+			throw new Error(
+				`Error fetching secrets from AWS: ${JSON.stringify(awsSecrets.Errors)}`,
+			);
+
+		for (const awsSecret of awsSecrets.SecretValues ?? []) {
+			if (awsSecret.Name && awsSecret.SecretString) {
+				const values = v.parse(
+					SecretsSchema,
+					JSON.parse(awsSecret.SecretString),
+				);
+
+				this.cachedSecrets.set(awsSecret.Name, new Map(Object.entries(values)));
+				this.isCached = true;
+			}
+		}
+
+		if (this.cachedSecrets.has(requestedSecret)) {
+			const value = this.cachedSecrets.get(requestedSecret)?.get(requestedKey);
+			if (value) {
+				return value;
+			}
+		}
+
+		throw new Error(`Secret ${key} not found in secret cache`);
 	}
 }
 
